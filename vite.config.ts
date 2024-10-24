419cda9a6f01730827b14c1c1e5a7ac250aae41c
4513ECHO <mail@4513echo.dev>
2024-10-24 22:52:34 +0900

    refactor: Apply deno fmt

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import deno from '@deno/vite-plugin'
-import build from '@hono/vite-build/deno'
-import adapter from '@hono/vite-dev-server/node'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import deno from "@deno/vite-plugin";
+import build from "@hono/vite-build/deno";
+import adapter from "@hono/vite-dev-server/node";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   cacheDir: "node_modules/.vite",
@@ -10,5 +10,5 @@ export default defineConfig({
     jsx: "automatic",
     jsxImportSource: "hono/jsx",
   },
-  plugins: [deno(), honox({ devServer: { adapter } }), build()]
-})
+  plugins: [deno(), honox({ devServer: { adapter } }), build()],
+});
